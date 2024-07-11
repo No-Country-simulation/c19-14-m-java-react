db45f2b77fa66776a84653b93066cee69e9271d7
Victor Raul <v1c70rotakmer@gmail.com>
2024-07-11 09:44:07 -0500

    feat: update landing pages components

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
