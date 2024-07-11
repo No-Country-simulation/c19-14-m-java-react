de5f391052c28c9b2e940d7da3e76eeb656ad7ec
Victor Raul <v1c70rotakmer@gmail.com>
2024-07-11 09:44:18 -0500

    chore: Update frontend files and configurations, add landing page components

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#FF6363",
-        secondary: {
-          100: "#E2E2D5",
-          200: "#888883",
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#E3D6F5',
+                secondary: '#8F5BD7',
+                tertiary: '#0A453A',
+                accent: '#F5E1DA',
+                dark: '#1A202C',
+            },
         },
-      },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
