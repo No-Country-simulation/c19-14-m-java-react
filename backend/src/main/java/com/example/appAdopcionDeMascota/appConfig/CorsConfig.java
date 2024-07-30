920ed6b1080c1ea3d6ee35c0ed8fe2bd73076743
leonardofvp <leonardovargasfp@gmail.com>
2024-07-29 21:16:52 -0300

    se añadio origin a cors cofig

--- backend/src/main/java/com/example/appAdopcionDeMascota/appConfig/CorsConfig.java
+++ backend/src/main/java/com/example/appAdopcionDeMascota/appConfig/CorsConfig.java
@@ -7,18 +7,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+                    @Override
+                    public void addCorsMappings(CorsRegistry registry) {
+                                    registry.addMapping("/**")
+                                            .allowedOrigins("http://localhost:3000",
+                                                    "https://petf1nder.vercel.app/",
+                                                    "http://localhost:4321/")
+                                            .allowedMethods("GET", "POST", "PUT", "DELETE")
+                                            .allowedHeaders("*")
+                                            .allowCredentials(true);
+                                            }
+                                                };
+        }
+
+
+
+
 }
